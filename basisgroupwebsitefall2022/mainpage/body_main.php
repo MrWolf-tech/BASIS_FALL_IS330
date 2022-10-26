2b82684944776f4943c343581a9c158b5d45b65c
MrWolf-tech <59813510+MrWolf-tech@users.noreply.github.com>
2022-10-25 18:08:11 -0700

    Upload of mainpage

---
+++ basisgroupwebsitefall2022/mainpage/body_main.php
@@ -0,0 +1,9 @@
+<!--This is the advertisement panel-->
+        <div class="adboard"></div>
+        <!--This is the twitter widget-->
+        <div class="twitterboard">
+                <script id="twitter-wjs" src="http://platform.twitter.com/widgets.js"></script>
+                <script charset="utf-8" src="https://platform.twitter.com/js/horizon_timeline.a7991bb824d62c8d5038ddd875db8389.js"></script>
+                <iframe scrolling="no" allowtransparency="true" src="https://platform.twitter.com/widgets/widget_iframe.7dae38096d06923d683a2a807172322a.html?origin=http%3A%2F%2Fkelcema.com" title="Twitter settings iframe" style="display: none;" frameborder="0"></iframe>
+                <iframe id="rufous-sandbox" scrolling="no" allowtransparency="true" allowfullscreen="true" style="position: absolute; visibility: hidden; display: none; width: 0px; height: 0px; padding: 0px; border: medium none;" title="Twitter analytics iframe" frameborder="0"></iframe>
+        </div>
---
+++ basisgroupwebsitefall2022/mainpage/header.php
@@ -0,0 +1,40 @@
+
+<html> <!--END TAG IN footer.php-->
+<head>
+    <title>CIS330_Group_Project</title>
+    <meta charset="UTF-8" />
+    <link rel="stylesheet" href="universal.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!--The scripts dont mean much here. But are here as a just in case.(For Jqueary and javascript)-->
+</head>
+
+<body> <!-- END TAG IN footer.php-->
+    <div class="main">
+    <header>
+        <!-- Banner -->
+        <div class="banner">
+            <img tag="logo" alt="Welcome to Kelcema Audio" src=""/>
+        </div>
+        
+        <!-- Navigation Bar Horizontal Fullscreen -->
+    <div class="Navbar">
+        <nav id="nav-container">
+            <div class="nav-item" href="index.php"><img src=""/><p>HOME</p></div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>ABOUT US</p>           </div> <!--PAGELINK needs file link-->
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>LIVE SOUND</p>         </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>OUR GEAR</p>           </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>RENTAL PRICING</p>     </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>DJ GEAR RENTALS</p>    </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>RETAIL SALES</p>       </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>PHOTO GALLERY</p>      </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>TOURING SERVICES</p>   </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>NEWS & EVENTS</p>      </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>EMERGENCY SERVICES</p> </div>
+            <div class="nav-item" style="float:right" href="PAGELINK"> <img src=""/> <p>CONTACT US</p>         </div>
+        </nav>
+    </div>
+    </header>
+
+
+
+
