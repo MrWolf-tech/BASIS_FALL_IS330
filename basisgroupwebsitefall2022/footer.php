4f249a0d0c5918f8946c6c0421e34c637791cffd
MrWolf-tech <59813510+MrWolf-tech@users.noreply.github.com>
2022-11-04 17:48:22 -0700

    Update

--- basisgroupwebsitefall2022/footer.php
+++ basisgroupwebsitefall2022/footer.php
@@ -1,5 +1,6 @@
     <footer>
         ©copyright 2001-2021 Kelcema Audio. All Rights Reserved. | 1122 Pike Street, PMB 671 | Seattle, WA 98122 || 11825 NE 113th St, Ste 132 | Vancouver, WA 98662
     </footer>
+</div>
 </body>
 </html>
--- basisgroupwebsitefall2022/header.php
+++ basisgroupwebsitefall2022/header.php
@@ -9,24 +9,31 @@
 </head>
 
 <body> <!-- END TAG IN footer.php-->
+    <div class="main">
     <header>
         <!-- Banner -->
-        <div class="banner"><img tag="logo" src=""/></div>
+        <div class="banner">
+            <img tag="logo" alt="Welcome to Kelcema Audio" src=""/>
+        </div>
+        
         <!-- Navigation Bar Horizontal Fullscreen -->
+    <div class="Navbar">
         <nav>
-            <div class="navbarbutton" href="index.php"><img src=""/><p>HOME</p></div>
-            <div class="navbarbutton" href="PAGELINK">ABOUT US</div> <!--PAGELINK needs file link-->
-            <div class="navbarbutton" href="PAGELINK">LIVE SOUND</div>
-            <div class="navbarbutton" href="PAGELINK">OUR GEAR</div>
-            <div class="navbarbutton" href="PAGELINK">RENTAL PRICING</div>
-            <div class="navbarbutton" href="PAGELINK">DJ GEAR RENTALS</div>
-            <div class="navbarbutton" href="PAGELINK">RETAIL SALES</div>
-            <div class="navbarbutton" href="PAGELINK">PHOTO GALLERY</div>
-            <div class="navbarbutton" href="PAGELINK">TOURING SERVICES</div>
-            <div class="navbarbutton" href="PAGELINK">NEWS & EVENTS</div>
-            <div class="navbarbutton" href="PAGELINK">EMERGENCY SERVICES</div>
-            <div class="navbarbutton" href="PAGELINK">"CONTACT US"</div>
+            <ul id="nav-container">
+            <ul id="nav-container">
+            <li class="nav-item" href="index.php"><a><img src="menupics/home.jpg" alt="HOME" title="Home"/></a></li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/aboutus.jpg" alt="About Us" title="About Us"/></a></li> <!--PAGELINK needs file link-->
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/services.jpg" alt="Live Sound" title="Live Sound"/></a>         </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/calendar.jpg" alt="Gear" title="Gear"/></a>           </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/pen.jpg" alt="Retail Sales" title="Retail Sales"/></a>       </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/photogallery.jpg" alt="Photo Gallery" title="Photo Gallery"/></a>      </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/truck.jpg" alt="Touring Services" title="Touring Services"/></a>   </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/myspace.jpg" alt="News & Events" title="News & Events"/></a>      </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/web-alert.jpg" alt="Emergency Services" title="Emergency Services"/></a> </li>
+            <li class="nav-item" style="float:right" href="PAGELINK">  <a><img src="menupics/contactus.jpg" alt="Contact Us" title="Contact Us"/></a>         </li>
+            </ul>
         </nav>
+    </div>
     </header>
 
 
