698b7bf2bc9801d00e19aa63539e3d936fd2fb6c
MrWolf-tech <59813510+MrWolf-tech@users.noreply.github.com>
2022-11-04 19:30:09 -0700

    Update

--- basisgroupwebsitefall2022/login.php
+++ basisgroupwebsitefall2022/login.php
@@ -1,8 +1,13 @@
-<div class="Login">
-    <form id="logfield">
-        <label for="username">UserName:</label><br>
-        <input type="text" id="fname" name="fname"><br>
-        <label for="Password">Password:</label><br>
-        <input type="password" id="password" name="password">
-    </form>
+<div class="login">
+        <div id="#actserv"><h1>Account Services</h1></div>
+        <div id="formcenter">
+            <form id="logfield">
+            <label for="username">UserName:</label><br>
+            <input type="text" id="fname" name="fname"><br>
+            <label for="Password">Password:</label><br>
+            <input type="password" id="password" name="password"><br /><br />
+            <input type="submit" id="submit" value="submit" />
+            </form>
+        </div>
+        <div id="noact"><a href="">Don't have an account? Click Here!</a></div>
 </div>
